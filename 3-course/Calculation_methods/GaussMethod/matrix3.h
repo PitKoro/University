9f66ec9330b66425d9cb627991d8e562f28695a9
Королев Пётр <37357614+lumberspin@users.noreply.github.com>
2020-02-04 22:32:46 +0300

    Done lab1

--- 3-course/Calculation_methods/GaussMethod/matrix3.h
+++ 3-course/Calculation_methods/GaussMethod/matrix3.h
@@ -1,7 +1,14 @@
 #pragma once
+#include<string>
+#include <functional>
+
 double** memoryAllocation(int rows, int lows); // Выделение памяти под  матрицу
 void readMatrixFromFile(double** arr, int rows, int lows); // Чтение матрицы из файла и запись в двумерный динамический массив
 void initialization_of_the_UnitMatrix(double** arr, int rows, int lows); // Инициализация единичной матрицы
 void print(double** arr, int rows, int lows); // Вывод матрицы на экран
 void matrixCombination(double** Matrix_Big, double** Matrix, double** UnitMatrix, int rows, int lows); // Соединяем данную матрицу с единичной
-double** Gauss_method(double** Matrix_Big, double** Matrix, int rows, int lows); // Метод Гаусса
+double** Gauss_method(double** Matrix_Big, double** Matrix, int rows, int lows, std::function<void(std::string, double**, int, int)> stepProcessor); // Метод Гаусса
+double** multiplyMatrix(double** Matrix, double** ResultMatrix, int rows);
+void swapRows(double* arr1, double* arr2, int N);
+void processEvent(std::string msg, double** Matrix_Big,int rows,int columns);
+void writingMatrixToFile(double** UnitToResultMatrix, int rows, int lows); //Записывает матрицу в файл

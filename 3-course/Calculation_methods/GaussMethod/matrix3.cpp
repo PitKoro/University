24cfd91bd718d4b390a3fcdda6dbdbac31378544
Королев Пётр <37357614+lumberspin@users.noreply.github.com>
2020-02-04 22:32:03 +0300

    Done lab1

--- 3-course/Calculation_methods/GaussMethod/matrix3.cpp
+++ 3-course/Calculation_methods/GaussMethod/matrix3.cpp
@@ -1,5 +1,8 @@
 #include<iostream>
 #include <fstream>
+#include <cmath>
+#include<cstdlib>
+
 #include "matrix3.h"
 using namespace std;
 
@@ -15,7 +18,7 @@ double** memoryAllocation(int rows, int lows)
 
 void readMatrixFromFile(double** arr, int rows, int lows)
 {
-	ifstream matrix3("matrix3.txt");
+	ifstream matrix3("C:\\Users\\Гость1\\Desktop\\DZ1\\matrix3.txt");
 	if (!matrix3.is_open())
 	{
 		cout << "*ERROR* FILE NOT OPENED" << endl;
@@ -49,14 +52,20 @@ void initialization_of_the_UnitMatrix(double** arr, int rows, int lows)
 	}
 }
 
-
 void print(double** arr, int rows, int lows)
 {
 	for (int i = 0; i < rows; i++)
 	{
 		for (int j = 0; j < lows; j++)
 		{
-			cout << "\t" << arr[i][j] << " ";
+		    if(abs(arr[i][j]) < 0.001)
+            {
+                cout << "\t" << 0 << " ";
+            }else
+            {
+                cout << "\t" << arr[i][j] << " ";
+            }
+
 		}
 		cout << endl;
 	}
@@ -75,36 +84,58 @@ void matrixCombination(double** Matrix_Big, double** Matrix, double** UnitMatrix
 	}
 }
 
-
-
-double** Gauss_method(double** Matrix_Big, double** Matrix, int rows, int lows)
+double** Gauss_method(double** Matrix_Big, double** Matrix, int rows, int lows, function<void(std::string, double**, int, int)> stepProcessor)
 {
+    double max ;
+
 	//Прямой ход (Зануление нижнего левого угла)
-	for (int k = 0; k < rows; k++) //k-номер строки
+	for (int k = 0; k < rows; k++) //k-номер столбца
 	{
-		for (int i = 0; i < 2 * lows; i++) //i-номер столбца
-			Matrix_Big[k][i] = Matrix_Big[k][i] / Matrix[k][k]; //Деление k-строки на первый член !=0 для преобразования его в единицу
+	    ///поиск максимального элемента в столбце и поменять местами сторчку с максимальным элементом с первой строчкой
+	    int pos = k;
+	    max = Matrix_Big[k][k];
+	    for(int i = k; i< rows; i++)
+        {
+            if(max < abs(Matrix_Big[i][k]))
+            {
+                max = abs(Matrix_Big[i][k]);
+                pos = i;
+
+            }
+        }
+
+	   swapRows(Matrix_Big[k],Matrix_Big[pos],2*rows);
+	   stepProcessor("Нашли главный элемент и поменяли его стоку с первой.", Matrix_Big, rows, 2*lows);
+
 		for (int i = k + 1; i < rows; i++) //i-номер следующей строки после k
 		{
 			double K = Matrix_Big[i][k] / Matrix_Big[k][k]; //Коэффициент
 			for (int j = 0; j < 2 * lows; j++) //j-номер столбца следующей строки после k
+			{
 				Matrix_Big[i][j] = Matrix_Big[i][j] - Matrix_Big[k][j] * K; //Зануление элементов матрицы ниже первого члена, преобразованного в единицу
+				stepProcessor("Выполнили преобразование(прямой ход)", Matrix_Big, rows, 2 * lows);
+			}
 		}
-		for (int i = 0; i < rows; i++) //Обновление, внесение изменений в начальную матрицу
-			for (int j = 0; j < lows; j++)
-				Matrix[i][j] = Matrix_Big[i][j];
 	}
 
 	//Обратный ход (Зануление верхнего правого угла)
 	for (int k = rows - 1; k > -1; k--) //k-номер строки
 	{
 		for (int i = 2 * lows - 1; i > -1; i--) //i-номер столбца
-			Matrix_Big[k][i] = Matrix_Big[k][i] / Matrix[k][k];
+		{
+			Matrix_Big[k][i] = Matrix_Big[k][i] / Matrix_Big[k][k];
+			stepProcessor("Выполнили преобразование1(обратный ход)", Matrix_Big, rows, 2 * lows);
+		}
+
 		for (int i = k - 1; i > -1; i--) //i-номер следующей строки после k
 		{
 			double K = Matrix_Big[i][k] / Matrix_Big[k][k];
 			for (int j = 2 * lows - 1; j > -1; j--) //j-номер столбца следующей строки после k
+			{
 				Matrix_Big[i][j] = Matrix_Big[i][j] - Matrix_Big[k][j] * K;
+				stepProcessor("Выполнили преобразование2(обратный ход)", Matrix_Big, rows, 2 * lows);
+			}
+				
 		}
 	}
 
@@ -114,4 +145,58 @@ double** Gauss_method(double** Matrix_Big, double** Matrix, int rows, int lows)
 		for (int j = 0; j < lows; j++)
 			InversMatrix[i][j] = Matrix_Big[i][j + lows];
 	return InversMatrix;
+}
+
+double** multiplyMatrix(double** Matrix, double** ResultMatrix, int rows)
+{
+    double** resultMultiplyMatrix = memoryAllocation(rows, rows);
+    for(int i = 0; i<rows; i++)
+    {
+        for(int j=0; j<rows; j++)
+        {
+            resultMultiplyMatrix[i][j] = 0;
+            for(int k=0; k<rows; k++)
+            {
+                resultMultiplyMatrix[i][j]  += Matrix[i][k]*ResultMatrix[k][j];
+            }
+        }
+    }
+    return resultMultiplyMatrix;
+}
+
+void swapRows(double* arr1, double* arr2, int N)
+{
+    double tmp;
+    for(int i=0;i<N;i++)
+    {
+        tmp = arr1[i];
+        arr1[i] = arr2[i];
+        arr2[i] = tmp;
+    }
+}
+
+void processEvent(string msg, double** Matrix_Big,int rows,int columns)
+{
+    cout << msg << endl;
+    print(Matrix_Big, rows, columns);
+    system("pause");
+}
+
+void writingMatrixToFile(double** UnitToResultMatrix, int rows, int lows)// Записывает Обратную Матрицу в Файл
+{
+	ofstream out; // поток для записи
+	std::string path = "C:\\Users\\Гость1\\Desktop\\DZ1\\inverSSMatrix.txt"; // путь до файла в котором хранится результат работы программы
+	out.open(path); // окрываем файл для записи
+	if (out.is_open())
+	{
+		for (int i = 0; i < rows; i++)
+		{
+			for (int j = 0; j < lows; j++)
+			{
+				out << UnitToResultMatrix[i][j] << "\t";
+			}
+			out << endl;
+		}
+	}
+
 }

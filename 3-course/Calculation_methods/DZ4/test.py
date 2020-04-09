b4f018700703806c375a2afa7adbc9d91554d167
lumberspin <pit.koro@gmail.com>
2020-04-09 14:59:46 +0300

    add comments

--- 3-course/Calculation_methods/DZ4/test.py
+++ 3-course/Calculation_methods/DZ4/test.py
@@ -5,9 +5,10 @@ def factorial(n):
         return 1
     return factorial(n-1) * n
 
-
+# Ряд Тейлора для f(x) = (x-sin(x))/x**3
 def fTeylor(x):
     return (1/factorial(3)) - ((x**2)/factorial(5)) + ((x**4)/factorial(7)) - ((x**6)/factorial(9))
 
+# Проинтегрированный ряд Тейлора
 def integralTeylor(start):
     return (start/factorial(3)) - ((start**3)/(factorial(5)*3)) + ((start**5)/(factorial(7)*5)) - ((start**7)/(factorial(9)*7))

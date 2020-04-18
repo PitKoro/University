bc56df65cbe4ecbfc84900530d7874c5fad50a74
lumberspin <pit.koro@gmail.com>
2020-04-18 20:12:49 +0300

    add comments

--- 3-course/Calculation_methods/DZ4/test.py
+++ 3-course/Calculation_methods/DZ4/test.py
@@ -12,3 +12,15 @@ def fTeylor(x):
 # Проинтегрированный ряд Тейлора
 def integralTeylor(start):
     return (start/factorial(3)) - ((start**3)/(factorial(5)*3)) + ((start**5)/(factorial(7)*5)) - ((start**7)/(factorial(9)*7))
+
+# x - аргумент, значение функции которого надо вычислить
+# eps - точность вычисления суммы
+def mySum(x, eps):
+    sum = 0
+    n=1
+    a_n=1
+    while abs(a_n)>eps:
+        sum += a_n; # суммируем очередной член ряда
+        n+=1 # переходим к следующему члену 
+        a_n *= x*n/(n-1); # пересчитываем a(n) через a(n-1)
+    return sum

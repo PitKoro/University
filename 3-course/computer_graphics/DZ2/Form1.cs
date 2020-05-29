4971ac655acd959157146cd6e374f2713d6b9c81
lumberspin <pit.koro@gmail.com>
2020-05-29 13:38:39 +0300

    done DZ2

---
+++ 3-course/computer_graphics/DZ2/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace GraphDZ2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public Form1(Bitmap img) {
+            InitializeComponent();
+            this.Size = img.Size;
+            pictureBox1.Size = img.Size;
+            pictureBox1.Image = img;
+        }
+    }
+}
